f98b57faad21f1840c964dd26c88a9c1cb5dff11
blackscure <wekesabuyahi@gmail.com>
2018-02-08 07:11:25 +0300

    Add the home route

---
+++ src/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)  # '__main__'
+
+
+@app.route('/')
+def hello_method():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(port=5000)
+
+
