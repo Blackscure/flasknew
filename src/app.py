a5bc90e2e7b6b1cf16ace320ab9a30f1f7087702
blackscure <wekesabuyahi@gmail.com>
2018-02-08 17:34:48 +0300

    add app.py

--- src/app.py
+++ src/app.py
@@ -3,9 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)  # '__main__'
 
 
-@app.route('/')
+@app.route('/home')
 def hello_method():
     return render_template('home.html')
+
+   
 
 if __name__ == '__main__':
     app.run(port=5000)
